1217fc8fdcde0bd79ba8c4f502bd6697ec99fefc
Neksten <denchik7366@gmail.com>
2023-11-25 21:57:39 +0400

    fix(common) изменил кнопку

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,17 +1,27 @@
-import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonHTMLAttributes, FC, ReactNode } from "react";
 import classNames from "classnames/bind";
 
 import styles from "./styles.module.scss";
 
 const cx = classNames.bind(styles);
 
-const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  reactAddon?: ReactNode;
+}
+
+const Button: FC<IButtonProps> = ({
+  reactAddon,
   children,
   className,
   ...props
 }) => (
-  <button type="button" className={cx("button", className)} {...props}>
+  <button
+    type="button"
+    className={cx("button", className, { icon: reactAddon })}
+    {...props}
+  >
     {children}
+    {reactAddon}
   </button>
 );
 
